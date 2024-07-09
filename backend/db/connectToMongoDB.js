d9423bb16045c5c34c54c9aa36645d0a22c11b55
rewaa1 <rewaayasser@hotmail.com>
2024-07-09 22:08:32 +0300

    new

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
-import dotenv from 'dotenv';
-import { API_URL } from '../config.js'
+  import mongoose from "mongoose";
+  import dotenv from 'dotenv';
+  import { API_URL } from '../config.js'
 
-const connectToMongoDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully!");
-  } catch (error) {
-    console.error("MongoDB connection error:", error.message);
-    }
-};
+  const connectToMongoDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URI);
+      console.log("MongoDB connected successfully!");
+    } catch (error) {
+      console.error("MongoDB connection error:", error.message);
+      }
+  };
 
-export default connectToMongoDB;
+  export default connectToMongoDB;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     port: 3000,
     proxy: {
-			"/api": {
-				target: "http://localhost:8000",
-			},
-		},
-	},
+      "/api": {
+        target: "http://localhost:8000",
+      },
+    },
+  },
+  build: {
+    outDir: 'dist'
+  }
 });
